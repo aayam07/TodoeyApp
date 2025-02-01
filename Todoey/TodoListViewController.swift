28dacd73cdfb63b87498d039334fb77c0a3c4562
Aayam Adhikari <thisisleo7@gmail.com>
2025-02-01 10:05:03 +0545

    Subclass UITableViewController and set up data source methods to display static text in table cells.

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK: - Tableview Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    // this function runs for as many times as the number of rows returned from the above method
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = itemArray[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+//        cell?.textLabel?.text = item // will be depriccated in the future iOS versions
+        
+        // Configure the cell's content
+        var cellContent = cell.defaultContentConfiguration()
+        cellContent.text = item
+        cell.contentConfiguration = cellContent
+        
+        return cell
+    }
+
+
+}
+
