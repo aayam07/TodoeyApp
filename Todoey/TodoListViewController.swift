66c981e93b325ae31866226be55d53d8f6668748
Aayam Adhikari <thisisleo7@gmail.com>
2025-02-01 13:10:06 +0545

    Implement TableView Delegate Methods and checkmarks.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -17,7 +17,7 @@ class TodoListViewController: UITableViewController {
         // Do any additional setup after loading the view.
     }
     
-    //MARK: - Tableview Datasource Methods
+    //MARK: - TableView Datasource Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -34,6 +34,23 @@ class TodoListViewController: UITableViewController {
         cell.contentConfiguration = cellContent
         
         return cell
+    }
+    
+    
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(itemArray[indexPath.row])
+        
+        // to add a checkmark when a cell gets selected and remove checkmark if already selected
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        // to provide gray background selection animation when clicking on a row
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
 
